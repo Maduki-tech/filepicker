f492406bff5e6af960c51da51a1f2d29563bd732
Maduki-tech <maduki.business@gmail.com>
2023-04-26 19:17:55 +0200

    add create (with bug)

--- src/pages/api/file-manager.ts
+++ src/pages/api/file-manager.ts
@@ -1,39 +1,77 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { type NextApiRequest, type NextApiResponse } from 'next'
 import SQLFileProvider from '../../utils/sql-file-provider'
 import { buffer } from 'micro'
 import querystring from 'querystring'
+import multer from 'multer'
+
+interface FileManagerFile {
+    name: string
+    size: number
+    dateModified: string
+    hasChild: boolean
+    isFile: boolean
+    type: string
+    filterPath: string
+}
+
+interface FileManagerResponse {
+    cwd: FileManagerFile
+    files: FileManagerFile[]
+}
+interface ReadAction {
+    action: 'read'
+    path?: string
+}
+
+interface OtherAction {
+    action: string
+    [key: string]: any
+}
+interface CreateAction {
+    action: 'create'
+    path: string
+    name: string
+    isFile: boolean
+    file?: Express.Multer.File
+}
+
+
+type FileManagerRequestBody = ReadAction | OtherAction | CreateAction
 
 export const config = {
     api: {
         bodyParser: false,
     },
 }
+const upload = multer()
 
 const sqlFileProvider = new SQLFileProvider()
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse
+    res: NextApiResponse<FileManagerResponse>
 ) {
-    // Check if the content type is JSON
-    const isJson = req.headers['content-type'] === 'application/json'
-
-    // Manually parse the request body as x-www-form-urlencoded or JSON
     const rawBody = (await buffer(req)).toString()
+
+    const isJson = req.headers['content-type'] === 'application/json'
     req.body = isJson ? JSON.parse(rawBody) : querystring.parse(rawBody)
 
-    const { method, query, body } = req
+    req.body = isJson ? JSON.parse(rawBody) : querystring.parse(rawBody)
+
+    const { method } = req
+    const body = req.body as FileManagerRequestBody
 
     if (method === 'POST') {
         const action = body.action
 
         switch (action) {
             case 'read':
-                const path = body.path || '/'
-                const fileList = await sqlFileProvider.getFileList(path)
+                const path = (body.path as string) || '/'
+                const fileList: FileManagerFile[] =
+                    await sqlFileProvider.getFileList(path)
                 console.log('fileList:', fileList)
                 // res.status(200).json(fileList);
-                res.status(200).json({
+                const response: FileManagerResponse = {
                     cwd: {
                         name: '/',
                         size: 0,
@@ -44,6 +82,34 @@ export default async function handler(
                         filterPath: '/',
                     },
                     files: fileList,
+                }
+                res.status(200).json(response)
+                break
+
+            case 'create':
+                const folderPath = body.path
+                const folderName = body.name
+                const newFolder = await sqlFileProvider.createFolder(
+                    folderPath,
+                    folderName
+                )
+                res.status(200).json(newFolder)
+                break
+            case 'upload':
+                // Handle file uploads with multer
+                upload.single('file')(req, res, async (err) => {
+                    if (err) {
+                        res.status(500).json({ error: err.message })
+                        return
+                    }
+
+                    const filePath = body.path
+                    const uploadedFile = req.file
+                    const newFile = await sqlFileProvider.createFile(
+                        filePath,
+                        uploadedFile
+                    )
+                    res.status(200).json(newFile)
                 })
                 break
             // Implement other actions like 'delete', 'rename', etc.
--- src/utils/sql-file-provider.ts
+++ src/utils/sql-file-provider.ts
@@ -1,8 +1,12 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, type Files } from '@prisma/client';
 
-const prisma = new PrismaClient();
 
 export default class SQLFileProvider {
+private prisma:  PrismaClient;
+
+constructor() {
+    this.prisma = new PrismaClient();
+}
   /**
    * @param {string | string[]} path - The path of the folder to get the file list.
    * @async
@@ -10,7 +14,7 @@ export default class SQLFileProvider {
    */
   async getFileList(path: string | string[]): Promise<any[]> {
     try {
-      const files = await prisma.files.findMany({
+      const files = await this.prisma.files.findMany({
         select: {
           Name: true,
           IsFile: true,
@@ -35,6 +39,50 @@ export default class SQLFileProvider {
       console.error('Error in getFileList:', error);
       return [];
     }
+  }
+
+  async createFolder(path: string, name: string): Promise<Files> {
+    const filterPath = path.endsWith('/') ? path : path + '/';
+    const newFolder = await this.prisma.files.create({
+      data: {
+        Name: name,
+        ParentID: null,
+        size: 0,
+        IsFile: false,
+        MimeType: null,
+        Content: null,
+        DateModified: new Date(),
+        DateCreated: new Date(),
+        HasChild: false,
+        IsRoot: false,
+        Type: 'Folder',
+        FilterPath: filterPath + name + '/',
+      },
+    });
+
+    return newFolder;
+  }
+
+ async createFile(path: string, file: Express.Multer.File): Promise<Files> {
+    const filterPath = path.endsWith('/') ? path : path + '/';
+    const newFile = await this.prisma.files.create({
+      data: {
+        Name: file.originalname,
+        ParentID: null,
+        size: file.size,
+        IsFile: true,
+        MimeType: file.mimetype,
+        Content: file.buffer,
+        DateModified: new Date(),
+        DateCreated: new Date(),
+        HasChild: false,
+        IsRoot: false,
+        Type: 'File',
+        FilterPath: filterPath,
+      },
+    });
+
+    return newFile;
   }
 
   // ... Implement other methods like createFolder, renameFile, remove, etc.
