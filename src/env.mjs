76b4134a6b72be3cbf4bc75b1810f771807c2761
Maduki-tech <maduki.business@gmail.com>
2023-05-19 23:21:52 +0200

    updated env

--- src/env.mjs
+++ src/env.mjs
@@ -1,4 +1,4 @@
-import { z } from 'zod'
+import { z } from 'zod';
 
 /**
  * Specify your server-side environment variables schema here. This way you can ensure the app isn't
@@ -8,7 +8,8 @@ const server = z.object({
     DATABASE_URL: z.string().url(),
     SHADOW_DB_URL: z.string(),
     NODE_ENV: z.enum(['development', 'test', 'production']),
-})
+    CLERK_SECRET_KEY: z.string().min(1),
+});
 
 /**
  * Specify your client-side environment variables schema here. This way you can ensure the app isn't
@@ -18,9 +19,15 @@ const client = z.object(
     /** @satisfies {Record<`NEXT_PUBLIC_${string}`, import('zod').ZodType>} */ ({
         NEXT_PUBLIC_LICENCE: z.string().min(1),
         NEXT_PUBLIC_URL: z.string().min(1),
+        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+        NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().min(1),
+        NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().min(1),
+        NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string().min(1),
+        NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z.string().min(1),
+
         // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
     })
-)
+);
 
 /**
  * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
@@ -34,44 +41,54 @@ const processEnv = {
     SHADOW_DB_URL: process.env.SHADOW_DB_URL,
     NEXT_PUBLIC_LICENCE: process.env.NEXT_PUBLIC_LICENCE,
     NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+        process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL:
+        process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL:
+        process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
+
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
-}
+};
 
 // Don't touch the part below
 // --------------------------
 
-const merged = server.merge(client)
+const merged = server.merge(client);
 
 /** @typedef {z.input<typeof merged>} MergedInput */
 /** @typedef {z.infer<typeof merged>} MergedOutput */
 /** @typedef {z.SafeParseReturnType<MergedInput, MergedOutput>} MergedSafeParseReturn */
 
-let env = /** @type {MergedOutput} */ (process.env)
+let env = /** @type {MergedOutput} */ (process.env);
 
 const skip =
     !!process.env.SKIP_ENV_VALIDATION &&
     process.env.SKIP_ENV_VALIDATION !== 'false' &&
-    process.env.SKIP_ENV_VALIDATION !== '0'
+    process.env.SKIP_ENV_VALIDATION !== '0';
 if (!skip) {
-    const isServer = typeof window === 'undefined'
+    const isServer = typeof window === 'undefined';
 
     const parsed = /** @type {MergedSafeParseReturn} */ (
         isServer
             ? merged.safeParse(processEnv) // on server we can validate all env vars
             : client.safeParse(processEnv) // on client we can only validate the ones that are exposed
-    )
+    );
 
     if (parsed.success === false) {
         console.error(
             '❌ Invalid environment variables:',
             parsed.error.flatten().fieldErrors
-        )
-        throw new Error('Invalid environment variables')
+        );
+        throw new Error('Invalid environment variables');
     }
 
     env = new Proxy(parsed.data, {
         get(target, prop) {
-            if (typeof prop !== 'string') return undefined
+            if (typeof prop !== 'string') return undefined;
             // Throw a descriptive error if a server-side env var is accessed on the client
             // Otherwise it would just be returning `undefined` and be annoying to debug
             if (!isServer && !prop.startsWith('NEXT_PUBLIC_'))
@@ -79,10 +96,10 @@ if (!skip) {
                     process.env.NODE_ENV === 'production'
                         ? '❌ Attempted to access a server-side environment variable on the client'
                         : `❌ Attempted to access server-side environment variable '${prop}' on the client`
-                )
-            return target[/** @type {keyof typeof target} */ (prop)]
+                );
+            return target[/** @type {keyof typeof target} */ (prop)];
         },
-    })
+    });
 }
 
-export { env }
+export { env };
